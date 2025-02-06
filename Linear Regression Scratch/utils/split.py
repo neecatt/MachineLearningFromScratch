f8f9e28bd315e99d9dba86d8ca3994a42335ced3
HorrorOTB <nicatabdullazade135@gmail.com>
2025-02-06 02:33:49 +0100

    fix index problem

--- Linear Regression Scratch/utils/split.py
+++ Linear Regression Scratch/utils/split.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def train_test_split(X, y, test_size=None, random_state=None, shuffle=None):
+def train_test_split(X, y, test_size=0.2, random_state=None, shuffle=True):
     
     X = np.asarray(X)
     y = np.asarray(y)
@@ -9,7 +8,12 @@ def train_test_split(X, y, test_size=None, random_state=None, shuffle=None):
     if len(X) != len(y):
         raise ValueError("Feature and target matrices should have the same length")
     
+    if test_size is None:
+        test_size = 0.2 
+        
     if isinstance(test_size, float):
+        if not 0 < test_size < 1:
+            raise ValueError("Float test size should be between 0 and 1")
         test_size = int(len(X) * test_size)
     elif isinstance(test_size, int):
         if test_size >= len(X):
@@ -18,7 +22,6 @@ def train_test_split(X, y, test_size=None, random_state=None, shuffle=None):
         raise ValueError("Test size should be a float or an integer")
     
     indices = np.arange(len(X))
-    print(indices)
     
     if shuffle:
         if random_state is not None:
@@ -32,6 +35,3 @@ def train_test_split(X, y, test_size=None, random_state=None, shuffle=None):
     y_train, y_test = y[train_indices], y[test_indices]
 
     return X_train, X_test, y_train, y_test
-            
-        
-#TODO: fix indexing error in this code
